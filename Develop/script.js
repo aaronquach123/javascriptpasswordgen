4560364b6da32967e94277f21e29cf93109fbcf9
aaron <aaronquach12341@gmail.com>
2021-02-13 11:01:31 -0800

    added functions for generating length and getting character type

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,50 @@
 // Assignment code here
+//Prompts for the length of the password
+var lengthOption = function() {
+  var lengthOptionPrompt = window.prompt("How many characters?");
+  //If not awnser correct return 
+  if (isNaN(lengthOptionPrompt) || lengthOptionPrompt === null || lengthOptionPrompt === "") {
+    window.alert("Please supply an integer number between 8 and 128.")
+    return lengthOption();
+  }
+  //turn back ito integer
+  lengthOption = parseInt(lengthOptionPrompt)
+}
+console.log(lengthOption)
+
+var characterChoice = function () {
+  var characterChoicePrompt = window.prompt("Enter '1' for lowercase '2' for upercase '3' for integers or '4' for symbols.");
+  characterChoicePrompt = parseInt(characterChoicePrompt);
+  switch (characterChoicePrompt) {
+    case 1:
+      characterChoice = "abcdefghijklmnopqrstuvwxyz";
+      console.log(characterChoice);
+      break;
+    case 2:
+      characterChoice = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      break;
+    case 3:
+      characterChoice = "0123456789"
+      break;
+    case 4:
+      characterChoice = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+      break;
+    default:
+      window.alert("You did not pick a valid answer please try again.");
+      return characterChoice();
+  }
+}
+
+var generatePassword = function() {
+  characterChoice();
+  lengthOption();
+  var password = ""
+  debugger;
+  for (var i = 0; i < lengthOption; i++) {
+    password += characterChoice.charAt(Math.floor(Math.random() * characterChoice.length));
+  }
+  console.log(password);
+}
 
 
 // Get references to the #generate element
@@ -10,7 +56,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  console.log(password);
 }
 
 // Add event listener to generate button
